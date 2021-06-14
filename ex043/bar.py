0ce25d73dcc5ca92ab2bc15673c2e22e6b514a3b
Fredrik Hårberg <harberg@kth.se>
2021-06-14 16:24:39 +0200

    Adding the progress-bar exercise

---
+++ ex043/bar.py
@@ -0,0 +1,12 @@
+
+current_number = 50
+total_numb = 100
+bar_length = 10
+
+
+if(current_number > 0 and current_number < total_numb):
+    print(f"""|{"="*(round(((current_number/total_numb)*bar_length))-1)}>{" "*round(((1-current_number/total_numb)*bar_length))}|""")
+elif current_number == 0:
+    print(f"""|{" " * bar_length}|""")
+else:
+    print(f"""|{"=" * bar_length}|""")  
