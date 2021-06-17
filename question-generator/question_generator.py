046040d9548d92cf8a9c236f5a62da5841ec6c69
Fredrik Hårberg <harberg@kth.se>
2021-06-17 13:37:17 +0200

    changed code

--- question-generator/question_generator.py
+++ question-generator/question_generator.py
@@ -1,16 +1,22 @@
 import random
 
-my_file = open("question.txt", encoding="utf8")
-content_list = my_file.readlines()
+try:
+    my_file = open("question.txt", encoding="utf8")
+    content_list = my_file.readlines()
 
-random.shuffle(content_list)
+    random.shuffle(content_list)
 
-print(f"""
---------The Question-of-the-day-generator has spoken!-------
-============================================================
-{content_list[0]}
-============================================================
-Total amount of questions in database: {len(content_list)}
-============================================================
-""")
+    print(f"""
+    --------The Question-of-the-day-generator has spoken!-------
+    ============================================================
+    {content_list[0]}
+    ============================================================
+    Total amount of questions in database: {len(content_list)}
+    ============================================================
+    """)
 
+except:
+    print("Something went wrong")
+
+finally:
+    my_file.close()
