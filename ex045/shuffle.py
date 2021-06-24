f43420ce31365b7f552523c5a51983b9ad08ffe5
Fredrik Hårberg <harberg@kth.se>
2021-06-24 09:26:26 +0200

    Updating code for better readability

--- ex045/shuffle.py
+++ ex045/shuffle.py
@@ -4,9 +4,9 @@ students = ["Adam","Bertil","Ceasar","David","Erik","Filip","Gustav","Helge","Iv
 
 random.shuffle(students)
 
-students.insert(0,"Group 1: ")
-students.insert(5,"Group 2: ")
-students.insert(10,"Group 3: ")
+students.insert(0,"-----Group 1----- ")
+students.insert(5,"-----Group 2----- ")
+students.insert(10,"-----Group 3----- ")
 
 for student in students:
     print(student)
